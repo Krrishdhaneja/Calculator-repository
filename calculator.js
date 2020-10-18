3974e2c1c5431f90051f4f4328e94ebee625dc29
Krrish Dhaneja <krrish21march@gmail.com>
2020-10-18 21:15:23 +0530

    Added some feature in calculator.js
    Added Square function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 let num1=window.prompt("type the num1");
 let num2=window.prompt("type the num2");
-let op = window.prompt("type the opeartor");
+let op = window.prompt("type the operator");
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
 if (op == '+') {
@@ -14,4 +14,14 @@ else if (op == 'x') {
 }
 else if (op == '/') {
 	alert(num1/num2);
+}
+else if (op == "sq") {
+	let num3=window.prompt("type 1 for num1 or 2 for num2");
+	num3 = parseFloat(num3);
+	if (num3 == 1) {
+		alert(num1*num1);
+	}
+	else if (num3 == 2) {
+		alert(num2*num2);
+	}
 }
